81e55fade2240e19ab376dc263085da97ebaa917
joranvar <bart.post@gmail.com>
2015-12-29 21:53:12 +0100

    Fix hlint and ghc warnings

--- Main.hs
+++ Main.hs
@@ -5,35 +5,35 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  file <- readFile (args !! 0)
+  file <- readFile $ head args
   let lp = preprocess (lines file)
-  let erg = (if elem "inflate" args then (inflate) else (compress)) lp
-  mapM_ putStrLn $ map concat erg
+  let erg = (if "inflate" `elem` args then inflate else compress) lp
+  mapM_ (putStrLn . concat) erg
 
 rpad :: Int -> String -> String
-rpad len string = take len $ string++(repeat ' ')
+rpad len string = take len $ string ++ repeat ' '
 
 --ensure last col has elements
 preprocess :: [String] -> [String]
-preprocess lines = map (rpad len) lines
-             where len = maximum $ map (length) lines
+preprocess ls = map (rpad len) ls
+  where len = maximum $ map length ls
 
 --calc lengths of the chunks(colname + spaces) for splitplaces
 dissect :: String -> [Int]
 dissect line = zipWith (\a b->length (a++b)) names spaces
-          where names = wordsBy (==' ') line
-                spaces= wordsBy (/=' ') (line++" ") --add " " to ensure space after last col-name
+  where names = wordsBy (==' ') line
+        spaces= wordsBy (/=' ') (line++" ") --add " " to ensure space after last col-name
 
 compress :: [String] -> [[String]]
-compress lines = add_del $ transpose $ map (shrinkcol) cols
-            where delim       = ';'
-                  head_lens   = dissect (head lines)
-                  cols        = transpose $ map (splitPlaces head_lens) lines
-                  shrinkcol c = map (\el -> reverse $ dropWhile (==' ') $ reverse $ dropWhile (==' ') el) c
-                  add_del     = map (map (++[delim]))
+compress ls = add_del $ transpose $ map shrinkcol cols
+  where delim     = ';'
+        head_lens = dissect (head ls)
+        cols      = transpose $ map (splitPlaces head_lens) ls
+        shrinkcol = map (reverse . dropWhile (==' ') . reverse . dropWhile (==' '))
+        add_del   = map (map (++[delim]))
 
 inflate :: [String] -> [[String]]
-inflate lines = transpose $ map padcol $ table
-          where table = transpose $ map (splitOn ";") lines --list of cols
-                padcol c = map (rpad m) c --pad whole col
-                   where m = (1+) $ maximum $ (map length) c --determine longest string in col, pad one more for prettiness
+inflate ls = transpose $ map padcol table
+  where table = transpose $ map (splitOn ";") ls --list of cols
+        padcol c = map (rpad m) c --pad whole col
+          where m = (1+) $ maximum $ map length c --determine longest string in col, pad one more for prettiness
